2e2bba56f682bdb417afb4743da28de400a41c8d
alex-nazemi <alexandernazemi@gmail.com>
2023-08-14 00:11:31 +0100

    changed conditional rendering to using ternary operator, added styling to p tag

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
-import InvestmentHeader from './Components/InvestmentHeader';
-import InvestmentResultsTable from './Components/InvestmentResultsTable';
-import InvestmentUserInputForm from './Components/InvestmentUserInputForm';
-import { useState } from 'react';
+import InvestmentHeader from "./Components/InvestmentHeader";
+import InvestmentResultsTable from "./Components/InvestmentResultsTable";
+import InvestmentUserInputForm from "./Components/InvestmentUserInputForm";
+import { useState } from "react";
 
 function App() {
-
   const [dataInputed, setDataInputed] = useState(false);
 
   const [userInput, setUserInput] = useState({
     currentSavings: "",
     yearlyContribution: "",
     expectedInterest: "",
-    investmentDuration: ""
+    investmentDuration: "",
   });
 
   const userInputHandler = (userData) => {
     setUserInput(userData);
     setDataInputed(true);
-  }
+  };
 
   const resetDataInputted = () => {
     setDataInputed(false);
-  }
+  };
 
   return (
     <div>
       <InvestmentHeader />
-      <InvestmentUserInputForm handler={userInputHandler} resetDataInputedHandler={resetDataInputted} />
-      {dataInputed && <InvestmentResultsTable userData={userInput} />}
+      <InvestmentUserInputForm
+        handler={userInputHandler}
+        resetDataInputedHandler={resetDataInputted}
+      />
+      {dataInputed ? (
+        <InvestmentResultsTable userData={userInput} />
+      ) : (
+        <p className="no-investment">No investment calculated yet.</p>
+      )}
     </div>
   );
 }
